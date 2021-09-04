546c1d66dd7e2ea1008976577606298033fed89a
bitcooker <dreamhighpassionhigh@gmail.com>
2021-09-03 19:19:04 -0500

    feat: 🎸 add main component

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-white">
+      <div className="py-16 sm:py-24 px-4 sm:px-6 lg:px-8 mx-auto max-w-screen-xl">
+        <div className="text-center">
+          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">Welcome to</h2>
+          <p className="my-3 text-4xl sm:text-5xl lg:text-6xl font-bold sm:tracking-tight text-gray-900">
+            reactjs-ts-vite-tailwindcss-boilerplate
+          </p>
+          <p className="text-xl text-gray-400">Start building for free.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
