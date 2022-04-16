fb02c1799a95bb6f736e2dd8cb30cfa795598870
bitcooker <dreamhighpassionhigh@gmail.com>
2022-04-16 15:36:12 -0500

    refactor: improve vite configuration (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,70 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { ConfigEnv, HtmlTagDescriptor, loadEnv, PluginOption, UserConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
+export default (config: ConfigEnv): UserConfig => {
+  /**
+   * The third argument is the prefix that is using to filter env vars
+   * from the process.env, by default 'VITE_'. So, the empty string
+   * will load all of them.
+   */
+  const rawEnv = loadEnv(config.mode, process.cwd(), '')
+
+  const plugins: (PluginOption | PluginOption[])[] = [reactRefresh()]
+
+  if (config.command === 'build') {
+    const hasGA4MeasurementId = !!rawEnv.VITE_GA4_MEASUREMENT_ID
+
+    const tags: HtmlTagDescriptor[] = []
+
+    if (hasGA4MeasurementId) {
+      tags.push(
+        {
+          injectTo: 'head',
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: `https://www.googletagmanager.com/gtag/js?id=${rawEnv.VITE_GA4_MEASUREMENT_ID}`,
+          },
+        },
+        {
+          injectTo: 'head',
+          tag: 'script',
+          children: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${rawEnv.VITE_GA4_MEASUREMENT_ID}');
+          `,
+        },
+      )
+    }
+
+    plugins.push(
+      createHtmlPlugin({
+        minify: true,
+        inject: { tags },
+      }),
+    )
+  }
+
+  return {
+    plugins,
+    server: {
+      port: Number(rawEnv.PORT) || 3000,
+      strictPort: true,
+      open: true,
     },
-  },
-})
+    build: {
+      outDir: 'app-build',
+      sourcemap: true,
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  }
+}
